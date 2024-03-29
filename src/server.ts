3b2274e0d0383cfd5576c5aadc2e8ed5f8ac2f85
Wanderley Cabral da Silva <39313251+cabraldasilvac@users.noreply.github.com>
2024-03-29 13:50:19 -0300

    Adicona nova configuração server.ts 7

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,7 @@ const start = async () => {
 
 start()
 
-export default async (req: any, res: any) => {
-    await app.ready()
-    app.server.emit('request', req, res)
-}
+// export default async (req: any, res: any) => {
+//     await app.ready()
+//     app.server.emit('request', req, res)
+// }
