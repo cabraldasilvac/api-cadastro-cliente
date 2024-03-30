0622133695bdab94c295c32e74f3911f4ce2279e
Wanderley Cabral da Silva <39313251+cabraldasilvac@users.noreply.github.com>
2024-03-30 19:26:05 -0300

    Altera a configuração de package.json  e server.ts v7

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,22 @@
-import Fastify from 'fastify'
+import Fastity from 'fastify'
 import cors from '@fastify/cors'
 import { routes } from './routes'
 
-const app = Fastify({ logger: true })
+const app = Fastity({ logger: true })
 
 app.setErrorHandler((error, request, reply) => {
     reply.code(400).send({ message: error.message })
 })
 
-// Registre o plugin CORS
-app.register(cors)
+const start = async () => {
+    await app.register(cors)
+    await app.register(routes)
 
-// Registre suas rotas
-app.register(routes)
+    try {
+        await app.listen({ port: 3333 })
+    } catch (err) {
+        process.exit(1)
+    }
+}
 
-// Não chame app.listen() diretamente
-
-// Exporte app para uso com Vercel
-export default app
+start()
