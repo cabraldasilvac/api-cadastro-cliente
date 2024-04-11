847497f234f2bb8d742247aed9a69d0bfef86864
WANDERLEY CABRAL <39313251+cabraldasilvac@users.noreply.github.com>
2024-04-11 10:21:53 -0300

    :rocket: feat: update routes.ts - replay to reply

--- src/routes.ts
+++ src/routes.ts
@@ -4,23 +4,23 @@ import { ListCustomersController } from './controllers/ListCustomersController'
 import { DeleteCustomerController } from './controllers/DeleteCustomerController'
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
-    fastify.get('/', async (request: FastifyRequest, replay: FastifyReply) => {
+    fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
         return { ok: true }
     })
 
-    fastify.get('/hello', async (request: FastifyRequest, replay: FastifyReply) => {
+    fastify.get('/hello', async (request: FastifyRequest, reply: FastifyReply) => {
         return { hello: 'Hello World!!' }
     })
 
-    fastify.post('/customer', async (request: FastifyRequest, replay: FastifyReply) => {
-        return new CreateCustomerController().handle(request, replay)
+    fastify.post('/customer', async (request: FastifyRequest, reply: FastifyReply) => {
+        return new CreateCustomerController().handle(request, reply)
     })
 
-    fastify.get('/customers', async (request: FastifyRequest, replay: FastifyReply) => {
-        return new ListCustomersController().handle(request, replay)
+    fastify.get('/customers', async (request: FastifyRequest, reply: FastifyReply) => {
+        return new ListCustomersController().handle(request, reply)
     })
 
-    fastify.delete('/customer', async (request: FastifyRequest, replay: FastifyReply) => {
-        return new DeleteCustomerController().handle(request, replay)
+    fastify.delete('/customer', async (request: FastifyRequest, reply: FastifyReply) => {
+        return new DeleteCustomerController().handle(request, reply)
     })
 }
